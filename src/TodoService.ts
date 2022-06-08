23e7a3dd3f7b2f101df8b73e69d90c58c3161010
mindtnv <mindtnv@gmail.com>
2022-06-08 21:37:33 +0300

    Add TodoService

---
+++ src/TodoService.ts
@@ -0,0 +1,46 @@
+﻿import { TaskService, TaskServiceEvent } from "./TaskService";
+import { LabelService } from "./LabelService";
+import { Label, Task, TodoistApi } from "@doist/todoist-api-typescript";
+
+export type TodoServiceEvent = TaskServiceEvent;
+
+export interface Todo extends Task {
+  labels: Label[];
+}
+
+export class TodoService {
+  private readonly api: TodoistApi;
+  tasks: TaskService;
+  labels: LabelService;
+
+  constructor(private readonly apiToken: string) {
+    this.api = new TodoistApi(apiToken);
+    this.tasks = new TaskService(this.api, {
+      pullInterval: 1000,
+    });
+    this.labels = new LabelService(this.api);
+  }
+
+  on(event: TodoServiceEvent, callback: (task: Todo, ...args: any[]) => any) {
+    this.tasks.on(event, async (task) => {
+      const labels: Label[] = [];
+      for (let labelId of task.labelIds) {
+        labels.push(await this.labels.resolve(labelId));
+      }
+      const todo = {
+        ...task,
+        labels,
+      };
+      callback(todo);
+    });
+  }
+
+  async start() {
+    await this.labels.pull();
+    this.tasks.start();
+  }
+
+  stop() {
+    this.tasks.stop();
+  }
+}

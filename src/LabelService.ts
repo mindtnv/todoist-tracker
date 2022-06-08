4a7ab5d04a1644f4596ec03595fe88c3ca8b34ad
mindtnv <mindtnv@gmail.com>
2022-06-08 21:36:40 +0300

    Add LabelService

---
+++ src/LabelService.ts
@@ -0,0 +1,26 @@
+﻿import { Label, TodoistApi } from "@doist/todoist-api-typescript";
+
+export class LabelService {
+  private readonly api: TodoistApi;
+  private store = new Map<number, Label>();
+
+  constructor(apiOrToken: TodoistApi | string) {
+    if (typeof apiOrToken === "string") this.api = new TodoistApi(apiOrToken);
+    else this.api = apiOrToken;
+  }
+
+  async pull() {
+    const labels = await this.api.getLabels();
+    labels.forEach((label) => {
+      this.store.set(label.id, label);
+    });
+  }
+
+  async resolve(id: number): Promise<Label> {
+    if (!this.store.has(id)) {
+      this.store.set(id, await this.api.getLabel(id));
+    }
+
+    return this.store.get(id)!;
+  }
+}

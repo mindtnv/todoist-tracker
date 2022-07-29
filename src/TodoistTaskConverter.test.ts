8b9b7c7785e4acfecbfb4901e5c23d5758761fdb
mindtnv <mindtnv@gmail.com>
2022-07-29 17:22:29 +0300

    Add TodoistTaskConverter

---
+++ src/TodoistTaskConverter.test.ts
@@ -0,0 +1,59 @@
+﻿import { CommonTask } from "./types";
+import { todoistTaskConverter } from "./TodoistTaskConverter";
+import { Task } from "@doist/todoist-api-typescript";
+
+describe("Common logic", () => {
+  test("Verbose task", () => {
+    const task: any = {
+      assignee: 2671362,
+      commentCount: 10,
+      completed: false,
+      content: "Buy Milk",
+      description: "",
+      due: {
+        date: "2016-09-01",
+        recurring: false,
+        datetime: "2016-09-01T09:00:00Z",
+        string: "tomorrow at 12",
+        timezone: "Europe/Moscow",
+      },
+      id: 2995104339,
+      labelsId: [2156154810, 2156154820, 2156154826],
+      order: 1,
+      priority: 1,
+      projectId: 2203306141,
+      sectionId: 7025,
+      parentId: 2995104589,
+      url: "https://todoist.com/showTask?id=2995104339",
+    };
+
+    const commonTaskWithoutLabels: CommonTask = {
+      order: 1,
+      priority: 1,
+      recurring: false,
+      due: {
+        date: new Date("2016-09-01T09:00:00Z"),
+      },
+      id: 2995104339,
+      labels: [],
+      title: "Buy Milk",
+    };
+
+    // Due copy
+    const commonTaskWithLabels: CommonTask = {
+      ...commonTaskWithoutLabels,
+      labels: ["a", "b"],
+    };
+
+    expect(
+      todoistTaskConverter(task as Task, {
+        labels: [],
+      })
+    ).toEqual(commonTaskWithoutLabels);
+    expect(
+      todoistTaskConverter(task as Task, {
+        labels: ["a", "b"],
+      })
+    ).toEqual(commonTaskWithLabels);
+  });
+});
---
+++ src/TodoistTaskConverter.ts
@@ -0,0 +1,26 @@
+﻿import { Task } from "@doist/todoist-api-typescript";
+import { CommonTask, TaskMeta } from "./types";
+
+export const todoistTaskConverter = (
+  task: Task,
+  meta: TaskMeta
+): CommonTask => {
+  const result: CommonTask = {
+    id: task.id,
+    title: task.content,
+    labels: meta.labels,
+    order: task.order,
+    priority: task.priority,
+    recurring: false,
+    due: undefined,
+  };
+
+  if (task.due) {
+    result.recurring = task.due.recurring;
+    result.due = {
+      date: new Date(task.due.datetime ?? task.due.date),
+    };
+  }
+
+  return result;
+};
